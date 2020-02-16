5d2daf128c6fd3281d96866ae5072179ec1c48e5
Ezequias <ezequias.net@gmail.com>
2020-02-16 18:41:00 -0300

    es6 tidy-up

--- http-service/routes/clients.js
+++ http-service/routes/clients.js
@@ -1,7 +1,7 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-router.get('/clients', function(req, res, next) {
+router.get('/clients', (req, res, next) => {
   res.send('respond with clients');
 });
 
