f30e355f09b2960369b181c04f5d19cb6a49604e
Ezequias <ezequias.net@gmail.com>
2020-02-18 11:29:19 -0300

    Better error handling

--- tests/routes/policies.test.js
+++ tests/routes/policies.test.js
@@ -3,10 +3,14 @@ const app = require('../../lib/app');
 const request = supertest(app);
 
 describe('GET /policies', () => {
-  it('should return a JSON array', async () => {
+  it('should require authentication', async () => {
     const res = await request.get('/policies');
-    expect(res.status).toBe(200);
-    expect(res.header['content-type']).toBe('application/json; charset=utf-8');
-    expect(res.body instanceof Array).toBe(true);
+    expect(res.status).toBe(401);
   });
+  // it('should return a JSON array', async () => {
+  //   const res = await request.get('/policies');
+  //   expect(res.status).toBe(200);
+  //   expect(res.header['content-type']).toBe('application/json; charset=utf-8');
+  //   expect(res.body instanceof Array).toBe(true);
+  // });
 });
