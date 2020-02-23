fb691ba9cd68dc9bc020c1008a4d8dc21fd1f4ab
Ezequias <ezequias.net@gmail.com>
2020-02-23 00:39:34 -0300

    Add policies filtered and paginated listing

--- __tests__/routes/login.test.js
+++ __tests__/routes/login.test.js
@@ -1,7 +1,8 @@
+jest.mock('../../lib/models');
+
 const supertest = require('supertest');
 const jwt = require('jsonwebtoken');
 
-jest.mock('../../lib/models');
 const { Client } = require('../../lib/models');
 const hash = require('../../lib/hash');
 const app = supertest(require('../../lib/app'));
@@ -27,14 +28,16 @@ describe('POST /login', () => {
     expect(res.status).toBe(400);
   });
   describe('with credentials,', () => {
-    beforeAll(() => Client.base.connect());
-    afterAll(() => Client.base.disconnect());
-    beforeEach(async () => {
+    beforeAll(async () => {
+      await Client.base.connect();
       const pwd = await hash.generate('a');
       const doc = new Client({ _id: 'a', email: 'a@a.a', pwd, role: 'user' });
       await doc.save();
     });
-    afterEach(async () => await Client.deleteMany());
+    afterAll(async () => {
+      await Client.deleteMany();
+      await Client.base.disconnect();
+    });
     it('with unknown email should return 401', async () => {
       const res = await app
         .post('/login')
